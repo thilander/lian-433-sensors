e6084bb8cc75803011b81a17a7d49b795751cf5a
Andreas Thilander <andreasthilander@pm.me>
2018-09-26 23:05:27 +0200

    Use enkelfnutts

--- sensors.py
+++ sensors.py
@@ -3,10 +3,10 @@
 import json
 import requests
 
-with open("secrets.json", "r") as s:
+with open('secrets.json', 'r') as s:
     secrets = json.load(s)
 
-with open("config.json", "r") as c:
+with open('config.json', 'r') as c:
     config = json.load(c)
 
 
@@ -14,19 +14,19 @@ with open("config.json", "r") as c:
 
 # balcony button
 
-device_name = "closet_door"
-friendly_name = "Closet door"
-device_class = "door"
-state = "ON"
+device_name = 'closet_door'
+friendly_name = 'Closet door'
+device_class = 'door'
+state = 'ON'
 
-api_url = "{0}/states/binary_sensor.{1}".format(config["api_base_url"], device_name)
-payload = {"state": state, "attributes": {
-    "friendly_name": friendly_name, 
-    "device_class": device_class}}
+api_url = '{0}/states/binary_sensor.{1}'.format(config['api_base_url'], device_name)
+payload = {'state': state, 'attributes': {
+    'friendly_name': friendly_name, 
+    'device_class': device_class}}
 
 response = requests.post(
     api_url,
-    headers={'x-ha-access': s["ha_api_password"], 'content-type': 'application/json'},
+    headers={'x-ha-access': s['ha_api_password'], 'content-type': 'application/json'},
     data=json.dumps(payload))
 
 print(response.text)
