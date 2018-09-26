e1111d6f6487d80db109813b03342c83fa34fffd
Andreas Thilander <andreasthilander@pm.me>
2018-09-26 23:26:08 +0200

    Figuring out tellstick events...

--- sensors.py
+++ sensors.py
@@ -2,31 +2,78 @@
 
 import json
 import requests
+import time
+import tellcore.telldus as td
+import tellcore.constants as const
 
+# Read secrets
 with open('secrets.json', 'r') as s:
     secrets = json.load(s)
 
+# Read config
 with open('config.json', 'r') as c:
     config = json.load(c)
 
+# sensor config. The keys are the id from the tellstick config and the value are the data for home assistant.
+sensor_mapping = config["sensor_mapping"]
 
-# closet door
+METHODS = {const.TELLSTICK_TURNON: 'turn on',
+           const.TELLSTICK_TURNOFF: 'turn off',
+           const.TELLSTICK_BELL: 'bell',
+           const.TELLSTICK_TOGGLE: 'toggle',
+           const.TELLSTICK_DIM: 'dim',
+           const.TELLSTICK_LEARN: 'learn',
+           const.TELLSTICK_EXECUTE: 'execute',
+           const.TELLSTICK_UP: 'up',
+           const.TELLSTICK_DOWN: 'down',
+           const.TELLSTICK_STOP: 'stop'}
 
-# balcony button
+# lastClick = time.time()
 
-device_name = 'closet_door'
-friendly_name = 'Closet door'
-device_class = 'door'
-state = 'ON'
+def event(id, method, data, cid):
+    method_string = METHODS.get(method, "UNKNOWN METHOD {0}".format(method))
+    print("{0}".format(id))
+    print("{0}".format(method_string))
+    print("{0}".format(data))
+    print("{0}".format(cid))
 
-api_url = '{0}/states/binary_sensor.{1}'.format(config['api_base_url'], device_name)
-payload = {'state': state, 'attributes': {
-    'friendly_name': friendly_name, 
-    'device_class': device_class}}
 
-response = requests.post(
-    api_url,
-    headers={'x-ha-access': secrets['ha_api_password'], 'content-type': 'application/json'},
-    data=json.dumps(payload))
+try:
+    import asyncio
+    loop = asyncio.get_event_loop()
+    dispatcher = td.AsyncioCallbackDispatcher(loop)
+    core = td.TelldusCore(callback_dispatcher=dispatcher)
+except ImportError:
+    loop = None
+    core = td.TelldusCore()
 
-print(response.text)
+core.register_device_event(event)
+
+try:
+    if loop:
+        loop.run_forever()
+    else:
+        import time
+        while True:
+            core.callback_dispatcher.process_pending_callbacks()
+            time.sleep(0.5)
+except KeyboardInterrupt:
+    pass
+
+
+# device_name = 'closet_door'
+# friendly_name = 'Closet door'
+# device_class = 'door'
+# state = 'ON'
+
+# api_url = '{0}/states/binary_sensor.{1}'.format(config['api_base_url'], device_name)
+# payload = {'state': state, 'attributes': {
+#     'friendly_name': friendly_name, 
+#     'device_class': device_class}}
+
+# response = requests.post(
+#     api_url,
+#     headers={'x-ha-access': secrets['ha_api_password'], 'content-type': 'application/json'},
+#     data=json.dumps(payload))
+
+# print(response.text)
